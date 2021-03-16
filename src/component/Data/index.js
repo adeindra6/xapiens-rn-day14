4743614c4d864029d63888ad9e1ee5222f1a92b8
Ade Indra Pratama <adeprathama@gmail.com>
2021-03-16 15:45:54 +0700

    Perbaikan data posts

--- src/component/Data/index.js
+++ src/component/Data/index.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component, useState, useEffect } from 'react';
 import {
     View,
     SafeAreaView,
@@ -6,36 +6,33 @@ import {
     Text,
     StyleSheet,
     FlatList,
+    ScrollView,
 } from 'react-native';
 import axios from 'axios';
-import { useState } from 'react';
-import data_posts from '../../../posts.json';
 
-
-const Item = ({ item, onPress }) => {
+const Item = ({ item, onPress }) => (
     <TouchableOpacity
         onPress={ onPress }
         style={ styles.btn }>
         <Text style={ styles.label }>{ item.title }</Text>
     </TouchableOpacity>
-};
+);
 
 const DataComponent = () => {
     const [ selectedId, setSelectedId ] = useState(null);
     const [ posts, setPosts ] = useState(null);
-    let data = [];
-
-    console.log(data_posts[0].title);
 
     const getData = async () => {
         await axios.get(`https://jsonplaceholder.typicode.com/posts`)
             .then(response => {
                 setPosts(response.data);
-                data.push(response.data);
-                console.log(data);
             }
         );
     };
+
+    useEffect(() => {        
+        getData();
+    }, []);
 
     const renderItem = ({ item }) => {
         return(
@@ -47,22 +44,42 @@ const DataComponent = () => {
     };
 
     return(
-        <View style={ styles.body }>
-            <Text style={ styles.title }>Data Posts</Text>
-            <SafeAreaView>
-                <FlatList
-                    data={data_posts[0]}
-                    renderItem={ renderItem }
-                    keyExtractor={(item) => item.id}
-                    extraData={selectedId}
-                />
-            </SafeAreaView>
+        <View>
+            <Text style={ styles.subtitle }>Data Posts</Text>
+            <ScrollView>
+                <SafeAreaView>
+                    <FlatList
+                        data={ posts }
+                        renderItem={ renderItem }
+                        keyExtractor={(item) => item.id}
+                        extraData={selectedId}
+                    />
+                </SafeAreaView>
+            </ScrollView>
         </View>
     );
 };
 
 const styles = StyleSheet.create({
+    subtitle: {
+        textAlign: 'center',
+        fontWeight: 'bold',
+        fontSize: 16,
+    },
 
+    btn: {
+        borderColor: 'black',
+        borderWidth: 1,
+        backgroundColor: '#5db075',
+        width: '80%',
+        alignSelf: 'center',
+    },
+
+    label: {
+        color: 'white',
+        textAlign: 'center',
+        fontSize: 16,
+    },
 });
 
 export default DataComponent;
